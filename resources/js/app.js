c0a7bf4224f90a83860b0edc93c3b2f0e78c3136
ederjgb94 <a2133330155@alumnos.uat.edu.mx>
2023-04-18 19:01:13 -0600

    boton + web + larevel + ruta web + bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+
+window.saludar = function () {
+    console.log('Hola mundo');
+}
---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <p>Hola M</p>
+
+    <button onclick="window.saludar()" type="button" class="btn btn-primary">Primary</button>
+
+
+    <script href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
